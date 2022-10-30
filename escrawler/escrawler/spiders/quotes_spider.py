83de6b70c91350be153aac8a9fd4fad4f8326d56
Mohammadreza Razzaghi <razzaghi229@gmail.com>
2022-10-30 06:16:24 +0330

    first commit

---
+++ escrawler/escrawler/spiders/quotes_spider.py
@@ -0,0 +1,28 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = []
+        for i in range(0, 2):
+            page = "o" + str(i * 10)
+            urls.append(
+                'https://www.tripadvisor.es/ShowTopic-g1-i12452-k3158072-' + page + '-Historias_de_ultramar-El_sofa_de_la_comunidad.html')
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for quote in response.css('div.postcontent'):
+            yield {
+                'postTitle': quote.css('div.postTitle::text').get(),
+                'postDate': quote.css('div.postDate::text').get(),
+                'postBody': quote.css('small.postBody::text').get(),
+            }
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
