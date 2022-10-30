62c0115cb881629121fb346e9d9325c29cb56490
Mohammadreza Razzaghi <razzaghi229@gmail.com>
2022-10-30 07:02:52 +0330

    change crawler

--- escrawler/escrawler/spiders/quotes_spider.py
+++ escrawler/escrawler/spiders/quotes_spider.py
@@ -7,7 +7,7 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = []
-        for i in range(0, 50):
+        for i in range(0, 200):
             page = "o" + str(i * 10)
             urls.append(
                 'https://www.tripadvisor.es/ShowTopic-g1-i12452-k3158072-' + page + '-Historias_de_ultramar-El_sofa_de_la_comunidad.html')
@@ -16,7 +16,7 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for quote in response.css('div.postcontent'):
+        for quote in response.css('div.post'):
             username_html = quote.css('div.username').extract()
             title_html = quote.css('div.postTitle').extract()[0]
             body_html = quote.css('div.postBody').extract()[0]
@@ -24,6 +24,7 @@ class QuotesSpider(scrapy.Spider):
             body_soup = BeautifulSoup(body_html)
             username = None
             if username_html:
+                pri
                 username_soup = BeautifulSoup(username_html[0])
                 username = username_soup.get_text()
             body = body_soup.get_text()
